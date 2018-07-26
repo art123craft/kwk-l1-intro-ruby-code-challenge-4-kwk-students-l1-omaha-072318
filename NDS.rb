0e936f8d596c096a1de8dbadc6e197672265a891
art123craft <cammiebluecammie@gmail.com>
2018-07-26 19:00:09 +0000

    automatically backed up by learn

---
+++ NDS.rb
@@ -0,0 +1,7 @@
+clothes = [["blouses", "t-shirts", "jackets"] ["jeans", "skirts","shorts"] ["sandals", "sneakers","heels"]]
+tops = ["blouses", "t-shirts", "jackets" ]
+bottoms = ["jeans", "skirts","shorts"]
+footwear = ["sandals", "sneakers","heels"]
+
+clothes = [tops, bottoms, footwear]
+puts clothes
